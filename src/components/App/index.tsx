eac2d24bec434a3e0773d38c73d8cf0c8c88d080
webmarkyn <web.markyn@gmail.com>
2020-07-21 22:11:00 +0300

    finish CollapsibleListItem

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,94 @@
-import React, {useEffect} from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-apollo";
-import {Container, LinearProgress} from "@material-ui/core";
-import {CONTINENTS} from "../../graphql/queries/Continents";
-import {Continents} from "../../graphql/queries/Continents/__generated__/Continents";
-import {Alert} from "@material-ui/lab";
+import { Container, LinearProgress, List, Paper } from "@material-ui/core";
+import { CONTINENTS } from "../../graphql/queries/Continents";
+import {
+  Continents,
+  Continents_continents,
+  Continents_continents_countries,
+  Continents_continents_countries_languages,
+} from "../../graphql/queries/Continents/__generated__/Continents";
+import { Alert } from "@material-ui/lab";
+import CollapsibleListItem from "../CollapsibleListItem";
+
+type CurrentItemType = {
+  continentId?: string;
+  countryId?: string;
+};
+
+type CurrentItemContextType = {
+  setCurrentItem?: (newVal: CurrentItemType) => void;
+} & CurrentItemType;
+
+export const CurrentItemContext = React.createContext<CurrentItemContextType>(
+  {}
+);
 
 export default function App() {
-  const {data, loading, error} = useQuery<Continents>(CONTINENTS);
+  const { data, loading, error } = useQuery<Continents>(CONTINENTS);
+  const [currentItem, setCurrentItem] = useState<CurrentItemType>({});
 
   const insides = () => {
-    if (loading) return (<LinearProgress />)
-    if (error) return (<Alert severity="error">Error occurred</Alert>)
-    return <h1>Hello</h1>
-  }
+    if (loading) return <LinearProgress />;
+    if (error || !data) return <Alert severity="error">Error occurred</Alert>;
+    const continents = data.continents;
+    return (
+      <Paper>
+        <List component="div" disablePadding>
+          {renderContinents(continents)}
+        </List>
+      </Paper>
+    );
+  };
+
+  const renderLanguage = (
+    languages: Continents_continents_countries_languages[],
+    continentId: string,
+    countryId: string
+  ) => {
+    return languages.map((language) => (
+      <CollapsibleListItem
+        text={language.name || ""}
+        continentId={continentId}
+        countryId={countryId}
+        key={language.code}
+      />
+    ));
+  };
+
+  const renderCountries = (
+    countries: Continents_continents_countries[],
+    continentId: string
+  ) => {
+    return countries.map((country) => (
+      <CollapsibleListItem
+        text={country.name}
+        collapsible
+        continentId={continentId}
+        countryId={country.code}
+        key={country.code}
+      >
+        {renderLanguage(country.languages, continentId, country.code)}
+      </CollapsibleListItem>
+    ));
+  };
+
+  const renderContinents = (continents: Continents_continents[]) => {
+    return continents.map((continent) => (
+      <CollapsibleListItem
+        text={continent.name}
+        collapsible
+        continentId={continent.code}
+        key={continent.code}
+      >
+        {renderCountries(continent.countries, continent.code)}
+      </CollapsibleListItem>
+    ));
+  };
 
   return (
-    <Container maxWidth="sm">
-      {insides()}
-    </Container>
-  )
+    <CurrentItemContext.Provider value={{ ...currentItem, setCurrentItem }}>
+      <Container maxWidth="sm">{insides()}</Container>
+    </CurrentItemContext.Provider>
+  );
 }
--- src/components/CollapsibleListItem/index.tsx
+++ src/components/CollapsibleListItem/index.tsx
@@ -1,41 +1,73 @@
-import React from "react";
+import React, { FunctionComponent, useContext, useEffect } from "react";
 import useCollapse from "../../hooks/useCollapse";
-import {Collapse, List, ListItem, ListItemText} from "@material-ui/core";
-import {ExpandLess, ExpandMore} from "@material-ui/icons";
+import { Collapse, List, ListItem, ListItemText } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+import { CurrentItemContext } from "../App";
 
 type Props = {
   text: string;
-  collapsible: boolean;
+  collapsible?: boolean;
   continentId: string;
   countryId?: string;
-  children: React.ReactChildren;
-}
+};
 
-export default function CollapsibleListItem({text, collapsible, continentId, countryId, children}: Props) {
-  const {open, collapse, close} = useCollapse();
+const CollapsibleListItem: FunctionComponent<Props> = ({
+  text,
+  collapsible,
+  continentId,
+  countryId,
+  children,
+}) => {
+  const { open, collapse, close } = useCollapse();
+  const currentItem = useContext(CurrentItemContext);
+
+  const handleCurrentItemChange = () => {
+    if (!currentItem || (currentItem.continentId === continentId && !countryId)) return;
+    if (currentItem.continentId !== continentId || currentItem.countryId !== countryId) close();
+  };
+
+  const handleClick = () => {
+    if (!collapsible || !currentItem || !currentItem.setCurrentItem) return;
+    if (!open)
+      currentItem.setCurrentItem({
+        continentId,
+        countryId: countryId || undefined,
+      });
+    console.log(currentItem);
+    collapse();
+  };
+
+  useEffect(() => {
+    handleCurrentItemChange();
+  }, [currentItem]);
 
   const renderChildren = () => {
     return (
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          {children}
-        </List>
+      <Collapse
+        in={open}
+        timeout="auto"
+        unmountOnExit
+        style={{ paddingLeft: "35px" }}
+      >
+        <List component="div">{children}</List>
       </Collapse>
-    )
-  }
+    );
+  };
 
   const renderArrow = () => {
     if (!collapsible) return null;
     return open ? <ExpandLess /> : <ExpandMore />;
-  }
+  };
 
   return (
     <div>
-      <ListItem button onClick={collapse}>
-        <ListItemText primary={text}/>
+      <ListItem button onClick={handleClick}>
+        <ListItemText primary={text} />
         {renderArrow()}
       </ListItem>
       {renderChildren()}
     </div>
-  )
-}
+  );
+};
+
+export default CollapsibleListItem;
